db66ac4e3fb714009e96f0da73b59bc314101d29
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-10-25 02:37:52 +0300

    Migrate addin project to .net6

--- SolutionName/Properties/AddinInfo.cs
+++ SolutionName/Properties/AddinInfo.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using Mono.Addins;
+﻿using Mono.Addins;
 using Mono.Addins.Description;
 
 [assembly: Addin(
@@ -12,16 +11,3 @@ using Mono.Addins.Description;
 [assembly: AddinCategory("IDE extensions")]
 [assembly: AddinDescription("Shows solution name in Mac app switcher and doc")]
 [assembly: AddinAuthor("DreamTeam Mobile, LLC")]
-
-[assembly: AssemblyTitle("DT.SolutionName")]
-[assembly: AssemblyDescription("Visual Studio for Mac Add-in to show current solution name")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("DreamTeam Mobile, LLC")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.2.*")]
-
-[assembly: AddinDependency("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
-[assembly: AddinDependency("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
